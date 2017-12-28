cf5484c6cbad539a4ac1d57f8764db821d0ca139
Mateus Calza <cto@prix.tech>
2017-12-27 22:39:20 -0200

    Vibrancy and devtools.

--- modules/renderer/index.js
+++ modules/renderer/index.js
@@ -1,11 +1,15 @@
 const bluebird = require('bluebird')
+const is = require('electron-is')
 const explorer = require('../explorer')
 
 async function init() {
-  debugger
 	const item = await explorer.fromPath('~')
 	await item.includeContent()
   console.log('item.content', item.content)
 }
 
 init()
+
+if (is.dev()) {
+  require('devtron').install()
+}
